fdf9e7c40df3fcaf05421b57add85719e6de39a0
Rafael Morais <rafahgmorais@gmail.com>
2020-10-04 17:43:15 -0300

    added logout buttun to homescreen

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { IconButton } from 'react-native-paper';
 import HomeScreen from '../screens/HomeScreen';
 import AddRoomScreen from '../screens/AddRoomScreen';
 import RoomScreen from '../screens/RoomScreen';
+import { AuthContext } from './AuthProvider'
+import { View } from 'react-native';
 
 const ChatAppStack = createStackNavigator();
 const ModalStack = createStackNavigator();
 
 function ChatApp() {
+    const { logout } = useContext(AuthContext); 
     return (
         <ChatAppStack.Navigator
             screenOptions={{
@@ -23,13 +26,29 @@ function ChatApp() {
             }}>
                 <ChatAppStack.Screen name='Home' component={HomeScreen} options={({navigation}) => ({
                     headerTitleAlign: 'center',
+                    headerLeft: () => {
+                        <IconButton
+                            icon='logout'
+                            size={28}
+                            color='#FFFFFF'
+                            onPress={() => logout()}
+                        />
+                    },
                     headerRight: () => (
+                        <View style={{ flexDirection: 'row' }}>
                         <IconButton
                             icon='message-plus'
                             size={28}
                             color='#FFFFFF'
                             onPress={() => navigation.navigate('AddRoom')}
                         />
+                        <IconButton
+                            icon='logout'
+                            size={28}
+                            color='#FFFFFF'
+                            onPress={() => logout()}
+                        />
+                        </View>
                     ),
                 })} />
                 <ChatAppStack.Screen name='Room' component={RoomScreen} options={({route}) => ({
